2e6a09db08cf6083840c52211484840cd1b950f1
JAMES Romario <r.james.jls@gmail.com>
2026-09-16 14:46:13 +0200

    Correction du collage : respect de la structure et des espaces

--- src/components/CustomEditor.js
+++ src/components/CustomEditor.js
@@ -78,6 +78,9 @@ export default function CustomEditor({ value, onChange, style, className }) {
           },
         },
       },
+      clipboard: {
+        matchVisual: false,
+      }
     }),
     []
   );
